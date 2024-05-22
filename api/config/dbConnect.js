cd21880bbc7b130334d89720fafd326e0d05996c
poojashankar0305 <poojaarisella@gmail.com>
2024-05-22 14:29:30 -0700

    DB Dump added

--- api/config/dbConnect.js
+++ api/config/dbConnect.js
@@ -4,7 +4,7 @@ async function createConnection(){
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'cms@#123',
+        password: 'test@#123',
         database: 'finclutech',
       });
     return connection;
